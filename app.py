9740469a6b2f0885681f734fc3a1b0a6ac81583a
muditmaheshwari14 <86740556+muditmaheshwari14@users.noreply.github.com>
2024-07-22 22:02:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def set_background(png_file):
     </style>
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
-background_image_path = os.path.abspath("app_bg1.WEBP")
+background_image_path = app_bg1.webp
 
 # Set your background image path
 set_background(background_image_path)
